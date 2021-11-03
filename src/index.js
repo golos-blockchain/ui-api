05f44a858c168388d26c4b2e7a452c7bc1ba5b32
1aerostorm <aerostorm1@gmail.com>
2021-11-03 07:59:49 +0300

    Remove steemjs from route, refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter, Switch, } from 'react-router-dom'
+import createBrowserHistory from 'history/createBrowserHistory'
+import golos from 'golos-lib-js';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import Api from './pages/Api';
 import Method from './pages/ApiMethod';
-import { unregister as unregisterServiceWorker } from './registerServiceWorker'
+import { unregister as unregisterServiceWorker } from './registerServiceWorker';
 import './index.css';
-import { Route, BrowserRouter, Switch } from 'react-router-dom'
-import createBrowserHistory from "history/createBrowserHistory"
-import "bootstrap/dist/css/bootstrap.css"
-import golos from "golos-lib-js";
-import api from "./steemjs/api/index";
+import api from './steemjs/api/index';
 
-console.log("init websocket");
-golos.config.set("websocket", api.getDefaults().ws);
+console.log('init websocket');
+golos.config.set('websocket', api.getDefaults().ws);
 golos.api.start();
 
-const history = createBrowserHistory({basename: 'steemjs'});
+const history = createBrowserHistory();
 
 ReactDOM.render(
-  <BrowserRouter history={history} basename={'steemjs'}>
-  <Switch>
-      <Route exact path="/" component={(props) => <Api {...props} />} />
-      <Route exact path="/api" component={(props) => <Api {...props} />}/>
-      <Route path="/api/:api_name/:method_name" component={(props) => <Method {...props} />}/>
-  </Switch>
-  </BrowserRouter>,
-  document.getElementById('root')
-)
+    <BrowserRouter history={history}>
+    <Switch>
+        <Route exact path='/' component={(props) => <Api {...props} />} />
+        <Route exact path='/api' component={(props) => <Api {...props} />}/>
+        <Route path='/api/:api_name/:method_name' component={(props) => <Method {...props} />}/>
+    </Switch>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
 
 unregisterServiceWorker();
